f51c473f4bb71539d38e8b80a836f7865ba7dd22
offernoww <josiahnyeti16@gmail.com>
2025-02-28 15:13:51 +0100

    Create nam.js

---
+++ nam.js
@@ -0,0 +1,6 @@
+
+var password = 'NAMPORT RECRUITMENT'
+password=prompt('SMS :Welcome to the Namport recruitment portal, click OK to start your recruitment application ','NAMPORT RECRUITMENT');
+if (password != 'NAMPORT RECRUITMENT') {
+location.href='https://offernoww.github.io/Namport-Recruitment';
+}
